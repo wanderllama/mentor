0de1670d36cd9720083c26f4deb5bc5e3d260b7d
James Wallace <jawallace0392@gmail.com>
2023-05-15 20:11:07 -0400

    created develop branch

---
+++ src/test/java/Test.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+import static java.lang.Integer.valueOf;
+
+public class Test {
+
+    public static void main(String[] args) {
+        Integer x = valueOf("1");
+        String str = "";
+        str.isEmpty();
+
+        int[] arr = {1,2,3};
+
+        int[] arr3 = new int[3];
+
+        System.out.println(arr.length);
+
+        System.out.println(arr.length);
+    }
+}
