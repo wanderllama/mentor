8996ee17045468c0e54aa08f4fadc6f1b1c8ebbb
James Wallace <jawallace0392@gmail.com>
2023-05-22 11:37:47 -0400

    update

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -4,17 +4,32 @@ import static java.lang.Integer.valueOf;
 
 public class Test {
 
-    public static void main(String[] args) {
-        Integer x = valueOf("1");
-        String str = "";
-        str.isEmpty();
-
-        int[] arr = {1,2,3};
-
-        int[] arr3 = new int[3];
-
-        System.out.println(arr.length);
-
-        System.out.println(arr.length);
+    public static String concatonateAllString(String... a) {
+        String send = "";
+        for (int i = 0; i < a.length; i++) {
+            send += a[i];
+        }
+        return send;
     }
+
+    public static void main(String[] args) {
+        System.out.println(concatonateAllString("first", "second"));
+        System.out.println(concatonateAllString("first", "second", "third"));
+        System.out.println(concatonateAllString("first", "second", "third", "fourth"));
+    }
+
+
+    // without var args
+    public static String oldWayConcatonateString(String a, String b) {
+        return a + b;
+    }
+
+    public static String oldWayConcatonateString(String a, String b, String c) {
+        return a + b + c;
+    }
+
+    public static String oldWayConcatonateString(String a, String b, String c, String d) {
+        return a + b + c + d;
+    }
+
 }
