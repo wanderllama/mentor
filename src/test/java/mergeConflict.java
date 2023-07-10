f19cb884ea47ca6a8371be00c0881abfdd6a2527
James Wallace <jawallace0392@gmail.com>
2023-07-10 19:56:40 -0400

    git conflict

---
+++ src/test/java/mergeConflict.java
@@ -0,0 +1,10 @@
+public class mergeConflict {
+
+
+    // created by merge2
+    // more conflicts
+    // I want to do this but its right
+    // I want to do this but its wrong
+
+    // we both do the following
+}
